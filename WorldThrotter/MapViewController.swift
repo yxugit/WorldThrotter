a305dda5dfa4c23eee59cc8c086c022d0e97e777
Jingwen Zhang <jingwenzhang@Jingwens-MacBook-Air.local>
2018-03-02 18:16:43 -0800

    done with chapter 6

--- WorldThrotter/MapViewController.swift
+++ WorldThrotter/MapViewController.swift
@@ -7,8 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    // MARK: Properties
+    var mapView: MKMapView!
+    
+    // MARK: Functions
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+            case 0:
+                mapView.mapType = .standard
+            case 1:
+                mapView.mapType = .hybrid
+            case 2:
+                mapView.mapType = .satellite
+            default:
+                break
+        }
+    }
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Satellite", "Hybrid"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingContraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingContraint.isActive = true
+        trailingConstraint.isActive = true
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapVieController loaded its view")
